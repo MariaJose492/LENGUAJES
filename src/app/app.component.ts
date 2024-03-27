a52987794f4417d1762dd950743565585f5c855a
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2024-03-27 13:45:57 -0500

    Add route for PrincipalComponent and import CommonModule in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+  CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyectoLenguajes';
+  jsonData: any;
+
+  onFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      const file = inputElement.files[0];
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        this.jsonData = fileReader.result;
+      };
+      fileReader.readAsText(file);
+    }
+  }
 }
